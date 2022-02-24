b8c46b961b2faf5010e388556c022dc0bd429a38
Lazzollin <lazacuestalautaro@gmail.com>
2022-02-24 12:25:15 -0300

    🍆

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
-import ViewGame from './pages/ViewGame';
-import ViewCategory from './pages/ViewCategory';
+import Home from './pages/Home/Home';
+import ViewGame from './pages/ViewGame/ViewGame';
+import ViewCategory from './pages/Categories/ViewCategory';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -23,26 +23,64 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import Categories from './pages/Categories/Categories';
+import Admin from './pages/Admin/Admin';
+import Create from './pages/Admin/Create';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import { changeAuth } from './firebase/firebaseConfig';
+import { useEffect, useState } from 'react';
+import { User } from 'firebase/auth';
+import { Update } from './pages/Admin/update';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/" exact={true}>
-          <Redirect to="/home" />
-        </Route>
-        <Route path="/home" exact={true}>
-          <Home />
-        </Route>
-        <Route path="/game/:id">
-           <ViewGame />
-        </Route>
-        <Route path="/category/:category">
-           <ViewCategory />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [user, setUser] = useState<string>();
+  const [admin, setAdmin] = useState<boolean>(true);
+  const [profile, setProfile] = useState<User>();
+
+
+  useEffect(() => {
+    changeAuth(setUser, setAdmin, setProfile)
+  }, [])
+
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route path="/" exact={true}>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/admin" exact={true}>
+            <Admin />
+          </Route>
+          <Route path="/category" exact={true}>
+            <Categories />
+          </Route>
+          <Route path="/home" exact={true}>
+            <Home />
+          </Route>
+          <Route path="/game/:id" exact={true}>
+            <ViewGame />
+          </Route>
+          <Route path="/category/:category" exact={true}>
+            <ViewCategory />
+          </Route>
+          <Route path="/register" exact={true}>
+            { !user ? <Register /> : <Redirect to="/home" /> }
+          </Route>
+          <Route path="/login" exact={true}>
+            { !user ? <Login /> : <Redirect to="/home" /> }
+          </Route>
+          <Route path="/create" exact={true}>
+            { admin ? <Create /> : <Redirect to="/home" />}
+          </Route>
+          <Route path="/update/:id" exact={true}>
+            { admin ? <Update /> : <Redirect to="/home" />}
+          </Route>
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  )
+};
 
 export default App;
--- src/components/GameListItem.tsx
+++ src/components/GameListItem.tsx
@@ -1,30 +1,47 @@
 import {
+  IonButton,
   IonItem,
   IonLabel,
-  IonNote
+  IonNote,
   } from '@ionic/react';
-import { Game } from '../data/games';
+import { useHistory } from 'react-router';
+import { removeGame } from '../firebase/firebaseConfig';
+import { Game } from '../models/games';
 import './GameListItem.css';
 
 interface GameListItemProps {
   game: Game;
+  is_admin: boolean;
 }
 
-const GameListItem: React.FC<GameListItemProps> = ({ game }) => {
+const GameListItem: React.FC<GameListItemProps> = ({ game, is_admin }) => {
+  const game_id = game.id ? game.id : ' '
+  const history = useHistory()
+
+  function remove_game() {
+    removeGame(game_id).then(() => {
+      window.location.reload()
+    })
+  }
+  
   return (
+    <IonItem>
     <IonItem routerLink={`/game/${game.id}`} detail={false}>
       <IonLabel className="ion-text-wrap">
         <h2>
-          {game.fromName}
+          {game.name}
           <span className="date">
             <IonNote>Lanzamiento: {game.date}</IonNote>
           </span>
         </h2>
-        <img src={game.img} alt="ashi" height="50px" width="50px"/>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
+        
       </IonLabel>
+    </IonItem>
+    { is_admin ? <IonButton href={'/update/'+game.id}>Update</IonButton> : null }
+    { is_admin ? <IonButton color='danger' onClick={remove_game}>Delete</IonButton> : null }
     </IonItem>
   );
 };
